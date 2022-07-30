9913407aae2639948392d1784553060ada0b122c
Nora Liu <noraliu0830@gmail.com>
2022-07-30 23:50:12 +0800

    feat: initiate project

---
+++ src/helper/useChallenge.ts
@@ -0,0 +1,7 @@
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import momentumChainAbi from '../abis/momentumChain.json'
+
+export const useChallenge = () => {
+  // // TODO: implement useChallenge
+}
