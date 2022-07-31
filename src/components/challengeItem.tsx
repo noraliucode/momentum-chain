6c189211abaa338a2b946783196e9a3f94335ef2
Nora Liu <noraliu0830@gmail.com>
2022-07-31 23:10:05 +0800

    feat: add useChallenge

--- src/components/challengeItem.tsx
+++ src/components/challengeItem.tsx
@@ -1,1 +1,18 @@
-// TODO: implement item
+import { IChallenge } from 'types/types'
+
+interface IProps {
+  challengeData: IChallenge[]
+}
+
+export const PoolTableItem: React.FC<IProps> = ({ challengeData }) => {
+  return (
+    <div>
+      {challengeData.map((x) => (
+        <div>
+          Date: {new Date(x.timestamp.toNumber() * 1000).toDateString()}, Link:{' '}
+          {x.link}{' '}
+        </div>
+      ))}
+    </div>
+  )
+}
--- src/helper/useChallenge.ts
+++ src/helper/useChallenge.ts
@@ -1,7 +1,46 @@
+import { MOMENTUM_CHAIN_CONTRACT_ADDRESS } from '../constants'
 import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
+import { IChallenge } from 'types/types'
 import momentumChainAbi from '../abis/momentumChain.json'
 
+interface IState {
+  loading: boolean
+  challengeData: IChallenge[] | undefined
+}
+
 export const useChallenge = () => {
-  // // TODO: implement useChallenge
+  const [state, setState] = useState<IState>({
+    loading: true,
+    challengeData: undefined,
+  })
+
+  const loadInfo = async () => {
+    try {
+      const provider = await ethers.getDefaultProvider('goerli')
+      const momentumChainContract = new ethers.Contract(
+        MOMENTUM_CHAIN_CONTRACT_ADDRESS,
+        momentumChainAbi,
+        provider
+      )
+      const recordIndex = await momentumChainContract.recordIndex()
+      const indexArray = Array.from(Array(recordIndex).keys())
+      const challengeData: IChallenge[] = await Promise.all(
+        indexArray.map((x) => momentumChainContract.records(x))
+      )
+
+      setState({
+        loading: false,
+        challengeData,
+      })
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
+  useEffect(() => {
+    loadInfo()
+  }, [])
+
+  return { ...state, loadInfo }
 }
